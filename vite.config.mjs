027bcb40efc1cccdf8c8652e9e8bdfc9f8b4f328
Brycycle <44349959+Bryce-Davidson@users.noreply.github.com>
2025-03-18 11:33:37 -0700

    chore: external maincss

--- vite.config.mjs
+++ vite.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
 			external: [
 				"obsidian",
 				"electron",
+				"main.css",
 				"@codemirror/autocomplete",
 				"@codemirror/collab",
 				"@codemirror/commands",
